4823e9ef9bf6349d5eac75401baafecd835e1e0f
Kieran Senior <kieran@kieransenior.co.uk>
2011-10-05 21:52:06 +0100

    Initial application creation using rails creator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recycle::Application.config.secret_token = '37ad649be57a8ea26c9aa9aa82d721c8ece79896ceb2f861cacec07f57fde181c099e122a0984a07bb0498343d7842f3ee5b22c96cd81c4bc5a76dcc11e8f4be'
